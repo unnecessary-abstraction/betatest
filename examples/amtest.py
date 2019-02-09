7be49b9a56b4d96d7b083c480c92740b533d3bce
Paul Barker <paul@betafive.co.uk>
2019-02-09 17:25:15 +0000

    examples: Fix pylint errors in amtest example
    Signed-off-by: Paul Barker <paul@betafive.co.uk>

--- examples/amtest.py
+++ examples/amtest.py
@@ -11,31 +11,32 @@ import sys
 import unittest
 
 # Expect to find betatest in the working directory when running these examples
+# pylint: disable=wrong-import-position
 sys.path.insert(0, os.getcwd())
 from betatest.amtest import AMTestRunner
 
 class TestPasses(unittest.TestCase):
     def test_pass(self):
         pass
-    
+
     @unittest.skip('Skipped')
     def test_skip(self):
         pass
-    
+
     @unittest.expectedFailure
     def test_xfail(self):
         self.fail('Expected failure')
-    
+
 class TestFails(unittest.TestCase):
     def test_fail(self):
         self.fail('Unexpected failure')
-    
+
     @unittest.expectedFailure
     def test_xpass(self):
         pass
-    
+
     def test_error(self):
-        return 1.0 / 0.0
+        self.assertEqual(1.0/0, 1)
 
 # Allow a '--pass' argument to disable tests that will fail. This is useful for
 # running the example in CI.
